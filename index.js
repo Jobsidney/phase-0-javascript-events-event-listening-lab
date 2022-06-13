007336a3c4918498977fb6568dabd06c6c0c661e
Jobsidney <jobsidney67@gmail.com>
2022-06-13 14:51:57 +0300

    commit tree 34a034f3ef1d6d41c38d70d713178c656c614282

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function addingEventListener() {
     const input= document.getElementById('input');
-    input.addEventListener('click',alart);
+    input.addEventListener('click',()=>alert('hello user'));
     
 }
 addingEventListener();
 
 
-function alart() {
-    alert('hello user');
-}
+// function alart() {
+//     alert('hello user');
+// }
